27eda04349380655fa2316da45f5c925aa55d48c
Daniel Rees <daniel_james_rees@hotmail.com>
2023-07-23 20:18:44 -0400

    think I have sveltekit with skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+// Your selected Skeleton theme:
+import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+// This contains the bulk of Skeletons required styles:
+import '@skeletonlabs/skeleton/styles/skeleton.css';
+import "../app.postcss";
+
+</script>
+<slot></slot>
